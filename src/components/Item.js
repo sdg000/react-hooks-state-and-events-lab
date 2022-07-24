1105edc3c3f3fb35d3a5f705ce757e25db3d34d2
Seth David Gyimah <esec26@gmail.com>
2022-07-24 11:21:56 +0000

    state and events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  // store inCart value to state and set value to false
+  const [inCart, setInCart] = useState(false)
+
+  // function handle add items to cart, when called , sets "inCarts" value to "not false" 
+  function location(){
+    setInCart((inCart => !inCart))
+  }
+  
+  // variable to className , depending on whether "inCart" is "true" or "not"
+  const setClass = inCart ? "in-cart" : null
+
+  //button element with tenary expression to decide it's text content based on the boolean value of "inCart"
   return (
-    <li className="">
+    <li className={setClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={location}>{inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  //console.log(items)
+
+  //step 1: save selection to useState
+  //step 2: when selection changes (onChange), create a function to save new choice to selection.
+  //step 3: Filter all items/foods based on selection and save to "newArray"
+  //step 4: Map through "newArray" selected and display
+
+  //step 1
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  //step 2
+  function saveNewCategory(e){
+    setSelectedCategory(e.target.value)
+  }
+
+  //step 3
+  const categorySelection = items.filter((i) => {
+    if (selectedCategory === "All"){
+      return true
+    }else{
+      return i.category === selectedCategory
+    }
+  })
+
+
+  //step 4 display "new items" by Mapping through "newArray", and for each item in array, call "Item Component" to display it:
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={saveNewCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +40,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {categorySelection.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
